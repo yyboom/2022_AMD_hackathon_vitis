ccd5c643e28f9d3928bdb170c01aff04f6e839c7
cho yun young <yoonyoung37@naver.com>
2022-08-20 02:11:19 +0900

    first commit

---
+++ audio/Debug/src/helloworld.d
@@ -0,0 +1,72 @@
+src/helloworld.o src/helloworld.o: ../src/helloworld.c ../src/platform.h \
+ ../src/platform_config.h \
+ D:/Xilinx/final_final/workspace/top/export/top/sw/top/domain_ps7_cortexa9_0/bspinclude/include/xil_printf.h \
+ D:/Xilinx/final_final/workspace/top/export/top/sw/top/domain_ps7_cortexa9_0/bspinclude/include/xil_types.h \
+ D:/Xilinx/final_final/workspace/top/export/top/sw/top/domain_ps7_cortexa9_0/bspinclude/include/xparameters.h \
+ D:/Xilinx/final_final/workspace/top/export/top/sw/top/domain_ps7_cortexa9_0/bspinclude/include/xparameters_ps.h \
+ D:/Xilinx/final_final/workspace/top/export/top/sw/top/domain_ps7_cortexa9_0/bspinclude/include/bspconfig.h \
+ D:/Xilinx/final_final/workspace/top/export/top/sw/top/domain_ps7_cortexa9_0/bspinclude/include/xil_types.h \
+ D:/Xilinx/final_final/workspace/top/export/top/sw/top/domain_ps7_cortexa9_0/bspinclude/include/xstatus.h \
+ D:/Xilinx/final_final/workspace/top/export/top/sw/top/domain_ps7_cortexa9_0/bspinclude/include/xil_assert.h \
+ D:/Xilinx/final_final/workspace/top/export/top/sw/top/domain_ps7_cortexa9_0/bspinclude/include/xllfifo.h \
+ D:/Xilinx/final_final/workspace/top/export/top/sw/top/domain_ps7_cortexa9_0/bspinclude/include/xstreamer.h \
+ D:/Xilinx/final_final/workspace/top/export/top/sw/top/domain_ps7_cortexa9_0/bspinclude/include/xenv.h \
+ D:/Xilinx/final_final/workspace/top/export/top/sw/top/domain_ps7_cortexa9_0/bspinclude/include/xenv_standalone.h \
+ D:/Xilinx/final_final/workspace/top/export/top/sw/top/domain_ps7_cortexa9_0/bspinclude/include/xdebug.h \
+ D:/Xilinx/final_final/workspace/top/export/top/sw/top/domain_ps7_cortexa9_0/bspinclude/include/xllfifo_hw.h \
+ D:/Xilinx/final_final/workspace/top/export/top/sw/top/domain_ps7_cortexa9_0/bspinclude/include/xil_io.h \
+ D:/Xilinx/final_final/workspace/top/export/top/sw/top/domain_ps7_cortexa9_0/bspinclude/include/xil_printf.h \
+ D:/Xilinx/final_final/workspace/top/export/top/sw/top/domain_ps7_cortexa9_0/bspinclude/include/xstatus.h \
+ D:/Xilinx/final_final/workspace/top/export/top/sw/top/domain_ps7_cortexa9_0/bspinclude/include/xpseudo_asm.h \
+ D:/Xilinx/final_final/workspace/top/export/top/sw/top/domain_ps7_cortexa9_0/bspinclude/include/xreg_cortexa9.h \
+ D:/Xilinx/final_final/workspace/top/export/top/sw/top/domain_ps7_cortexa9_0/bspinclude/include/xpseudo_asm_gcc.h \
+ D:/Xilinx/final_final/workspace/top/export/top/sw/top/domain_ps7_cortexa9_0/bspinclude/include/xparameters.h \
+ D:/Xilinx/final_final/workspace/top/export/top/sw/top/domain_ps7_cortexa9_0/bspinclude/include/xil_io.h
+
+../src/platform.h:
+
+../src/platform_config.h:
+
+D:/Xilinx/final_final/workspace/top/export/top/sw/top/domain_ps7_cortexa9_0/bspinclude/include/xil_printf.h:
+
+D:/Xilinx/final_final/workspace/top/export/top/sw/top/domain_ps7_cortexa9_0/bspinclude/include/xil_types.h:
+
+D:/Xilinx/final_final/workspace/top/export/top/sw/top/domain_ps7_cortexa9_0/bspinclude/include/xparameters.h:
+
+D:/Xilinx/final_final/workspace/top/export/top/sw/top/domain_ps7_cortexa9_0/bspinclude/include/xparameters_ps.h:
+
+D:/Xilinx/final_final/workspace/top/export/top/sw/top/domain_ps7_cortexa9_0/bspinclude/include/bspconfig.h:
+
+D:/Xilinx/final_final/workspace/top/export/top/sw/top/domain_ps7_cortexa9_0/bspinclude/include/xil_types.h:
+
+D:/Xilinx/final_final/workspace/top/export/top/sw/top/domain_ps7_cortexa9_0/bspinclude/include/xstatus.h:
+
+D:/Xilinx/final_final/workspace/top/export/top/sw/top/domain_ps7_cortexa9_0/bspinclude/include/xil_assert.h:
+
+D:/Xilinx/final_final/workspace/top/export/top/sw/top/domain_ps7_cortexa9_0/bspinclude/include/xllfifo.h:
+
+D:/Xilinx/final_final/workspace/top/export/top/sw/top/domain_ps7_cortexa9_0/bspinclude/include/xstreamer.h:
+
+D:/Xilinx/final_final/workspace/top/export/top/sw/top/domain_ps7_cortexa9_0/bspinclude/include/xenv.h:
+
+D:/Xilinx/final_final/workspace/top/export/top/sw/top/domain_ps7_cortexa9_0/bspinclude/include/xenv_standalone.h:
+
+D:/Xilinx/final_final/workspace/top/export/top/sw/top/domain_ps7_cortexa9_0/bspinclude/include/xdebug.h:
+
+D:/Xilinx/final_final/workspace/top/export/top/sw/top/domain_ps7_cortexa9_0/bspinclude/include/xllfifo_hw.h:
+
+D:/Xilinx/final_final/workspace/top/export/top/sw/top/domain_ps7_cortexa9_0/bspinclude/include/xil_io.h:
+
+D:/Xilinx/final_final/workspace/top/export/top/sw/top/domain_ps7_cortexa9_0/bspinclude/include/xil_printf.h:
+
+D:/Xilinx/final_final/workspace/top/export/top/sw/top/domain_ps7_cortexa9_0/bspinclude/include/xstatus.h:
+
+D:/Xilinx/final_final/workspace/top/export/top/sw/top/domain_ps7_cortexa9_0/bspinclude/include/xpseudo_asm.h:
+
+D:/Xilinx/final_final/workspace/top/export/top/sw/top/domain_ps7_cortexa9_0/bspinclude/include/xreg_cortexa9.h:
+
+D:/Xilinx/final_final/workspace/top/export/top/sw/top/domain_ps7_cortexa9_0/bspinclude/include/xpseudo_asm_gcc.h:
+
+D:/Xilinx/final_final/workspace/top/export/top/sw/top/domain_ps7_cortexa9_0/bspinclude/include/xparameters.h:
+
+D:/Xilinx/final_final/workspace/top/export/top/sw/top/domain_ps7_cortexa9_0/bspinclude/include/xil_io.h:
---
+++ audio/Debug/src/platform.d
@@ -0,0 +1,16 @@
+src/platform.o src/platform.o: ../src/platform.c \
+ D:/Xilinx/final_final/workspace/top/export/top/sw/top/domain_ps7_cortexa9_0/bspinclude/include/xparameters.h \
+ D:/Xilinx/final_final/workspace/top/export/top/sw/top/domain_ps7_cortexa9_0/bspinclude/include/xparameters_ps.h \
+ D:/Xilinx/final_final/workspace/top/export/top/sw/top/domain_ps7_cortexa9_0/bspinclude/include/xil_cache.h \
+ D:/Xilinx/final_final/workspace/top/export/top/sw/top/domain_ps7_cortexa9_0/bspinclude/include/xil_types.h \
+ ../src/platform_config.h
+
+D:/Xilinx/final_final/workspace/top/export/top/sw/top/domain_ps7_cortexa9_0/bspinclude/include/xparameters.h:
+
+D:/Xilinx/final_final/workspace/top/export/top/sw/top/domain_ps7_cortexa9_0/bspinclude/include/xparameters_ps.h:
+
+D:/Xilinx/final_final/workspace/top/export/top/sw/top/domain_ps7_cortexa9_0/bspinclude/include/xil_cache.h:
+
+D:/Xilinx/final_final/workspace/top/export/top/sw/top/domain_ps7_cortexa9_0/bspinclude/include/xil_types.h:
+
+../src/platform_config.h:
